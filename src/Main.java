c86ddcaec330f5b120b3a7355237288f284dc271
mozyakov <s.mozyakov@gmail.com>
2023-03-23 23:33:21 +0700

    1 стрим сделан
    подсчет несовершеннолетних

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
 import java.util.*;
+import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 10_000_000; i++) {
+        for (int i = 0; i < 10; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
                     new Random().nextInt(100),
                     Sex.values()[new Random().nextInt(Sex.values().length)],
                     Education.values()[new Random().nextInt(Education.values().length)])
+
             );
         }
+        for (Person person : persons) { //пример чтоб вывести список
+            System.out.println(person);
+        }
+        long count = persons.stream() //стрим подсчета несовершеннолетних
+                .filter((age) -> age.getAge() < 18)
+                .count();
+        System.out.println(count); //итоговое кол-во несовершеннолетних
+
 
 
 
