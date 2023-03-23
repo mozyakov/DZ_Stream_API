10191629486d29323faff9a59ff6184767dfdd42
mozyakov <s.mozyakov@gmail.com>
2023-03-24 02:26:16 +0700

    Update Main.java
    Сделан второй стрим

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
@@ -12,17 +13,26 @@ public class Main {
                     new Random().nextInt(100),
                     Sex.values()[new Random().nextInt(Sex.values().length)],
                     Education.values()[new Random().nextInt(Education.values().length)])
-
             );
         }
         for (Person person : persons) { //пример чтоб вывести список
             System.out.println(person);
         }
-        long count = persons.stream() //стрим подсчета несовершеннолетних
+        System.out.println("=========");
+
+
+
+        List<Person> manForWar = persons.stream() //2 стрим подсчет призывников
+                        .filter((age) -> (age.getAge() >= 18) & (age.getAge() < 50))
+                        .filter((sex) -> sex.getSex().equals(Sex.MAN))
+                        .collect(Collectors.toList());
+        System.out.println(manForWar);
+        System.out.println("=========");
+        long countAgeLess18 = persons.stream() //1 стрим подсчет несовершеннолетних
                 .filter((age) -> age.getAge() < 18)
                 .count();
-        System.out.println(count); //итоговое кол-во несовершеннолетних
-
+        System.out.println(countAgeLess18);  //кол-во несовершеннолетних
+        System.out.println("=========");
 
 
 
